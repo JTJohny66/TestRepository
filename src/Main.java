a56d76a3f17578f2ba3d1805f22a949f18fa9ab2
Jonathan Williams <jtw663@gmail.com>
2025-09-04 18:40:10 -0400

    some-more-testing

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,7 @@ public class Main {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
 
-        list.addLast(10);
-        list.addLast(20);
-        list.addFirst(5);
-        list.printList();  // Output: 5 -> 10 -> 20 -> null
-        list.addLast(10);
-        list.addLast(20);
-        list.addFirst(5);
-        list.printList();  // Output: 5 -> 10 -> 20 -> null
+        list.addFirst(3);  // Output: 5 -> 10 -> 20 -> null
 
 
         list.removeFirst();
